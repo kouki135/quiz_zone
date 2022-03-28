7e78688eea3acac85c7b9c284883e5ed07661544
kouki135 <arumaziki2000@gmail.com>
2022-03-28 21:50:16 +0900

    トップページ骨子作成

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,2 +1,5 @@
 class QuizzesController < ApplicationController
+  def index
+   @quizzes = Quiz.all 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :quizzes, only: :index
+  root to: "quizzes#index"
 end
